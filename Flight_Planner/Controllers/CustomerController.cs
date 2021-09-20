3eb253ab5bb1a0e67fc801381a7ffeb47fb4ac89
AlexanderV <aleksandrs.verenkovs@gmail.com>
2021-09-20 13:30:38 +0300

    first phase

---
+++ Flight_Planner/Controllers/CustomerController.cs
@@ -0,0 +1,49 @@
+﻿using Flight_Planner.Models;
+using Flight_Planner.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Flight_Planner.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        [HttpGet]
+        [Route("airports")]
+
+        public IActionResult GetByTag(string search)
+        {
+            return Ok(FlightList.GetByTag(search));
+        }
+
+        [HttpPost]
+        [Route("flights/search")]
+        public IActionResult SearchFlights(FlightSearch search)
+        {
+            if (!FlightList.IsValidFlight(search))
+                return BadRequest();
+            if (!FlightList.IsSameAirport(search))
+                return BadRequest();
+
+            var response = FlightList.SearchResult(search);
+            return Ok(response);
+        }
+        [HttpGet]
+        [Route("flights/{id}")]
+        public IActionResult GetFlight(int id)
+        {
+            var flightId = FlightList.GetById(id);
+            if (flightId == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(flightId);
+        }
+    }
+}
---
+++ Flight_Planner/Models/Airport.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Flight_Planner.Models
+{
+    public class Airport
+    {
+        public string Country { get; set; }
+        public string City { get; set; }
+        [JsonPropertyName("airport")]
+        public string AirportCode { get; set; }
+
+    }
+}
